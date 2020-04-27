24991d486eefc69b2d342e03a1c52d76b265cceb
Dandrewd <amdiachenko@gmail.com>
2020-04-27 11:53:28 +0300

    Just simple for count

---
+++ 0_Home/count-digits.py
@@ -0,0 +1,33 @@
+'''
+You need to count the number of digits in a given string.
+
+Input: A Str.
+Output: An Int.
+
+Example:
+count_digits('hi') == 0
+count_digits('who is 1st here') == 1
+count_digits('my numbers is 2') == 1
+count_digits('This picture is an oil on canvas '
+ 'painting by Danish artist Anna '
+ 'Petersen between 1845 and 1910 year') == 8
+count_digits('5 plus 6 is') == 2
+count_digits('') == 0
+'''
+
+def count_digits(text: str):
+    counter = 0
+    for symb in text:
+        if symb.isdigit():
+            counter +=1
+
+    return counter
+
+assert count_digits('hi') == 0
+assert count_digits('who is 1st here') == 1
+assert count_digits('my numbers is 2') == 1
+assert count_digits('This picture is an oil on canvas '
+ 'painting by Danish artist Anna '
+ 'Petersen between 1845 and 1910 year') == 8
+assert count_digits('5 plus 6 is') == 2
+assert count_digits('') == 0
