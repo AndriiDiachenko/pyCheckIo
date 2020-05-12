d5dc660fe23bd67e59558cca5ce0d270977379e3
Dandrewd <amdiachenko@gmail.com>
2020-05-12 11:32:18 +0300

    little morse decoder and encoder

---
+++ 0_Home/morse-decoder.py
@@ -0,0 +1,29 @@
+'''
+Your task is to decrypt the secret message using the Morse code.
+The message will consist of words with 3 spaces between them and 1 space between each letter of each word.
+If the decrypted text starts with a letter then you'll have to print this letter in uppercase.
+
+morse_decoder("... --- -- .   - . -..- -") == "Some text"
+morse_decoder("..--- ----- .---- ---..") == "2018"
+morse_decoder(".. -   .-- .- ...   .-   --. --- --- -..   -.. .- -.--") == "It was a good day"
+'''
+
+def morse_decoder(message: str):
+    morse_dict = { 'A':'.-', 'B':'-...',
+                    'C':'-.-.', 'D':'-..', 'E':'.',
+                    'F':'..-.', 'G':'--.', 'H':'....',
+                    'I':'..', 'J':'.---', 'K':'-.-',
+                    'L':'.-..', 'M':'--', 'N':'-.',
+                    'O':'---', 'P':'.--.', 'Q':'--.-',
+                    'R':'.-.', 'S':'...', 'T':'-',
+                    'U':'..-', 'V':'...-', 'W':'.--',
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..',
+                    '1':'.----', '2':'..---', '3':'...--',
+                    '4':'....-', '5':'.....', '6':'-....',
+                    '7':'--...', '8':'---..', '9':'----.',
+                    '0':'-----', ', ':'--..--', '.':'.-.-.-',
+                    '?':'..--..', '/':'-..-.', '-':'-....-',
+                    '(':'-.--.', ')':'-.--.-'}
+
+    morse_ch_dict = dict((code, let) for let, code in morse_dict.items())
+
