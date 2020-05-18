884183daf7f8f841e1c6e26f0d280a1bb66e89eb
Dandrewd <amdiachenko@gmail.com>
2020-05-18 22:28:02 +0300

    with is alpha check

---
+++ 1_Elementary/all-upper.py
@@ -0,0 +1,32 @@
+'''
+Check if a given string has all symbols in upper case.
+If the string is empty or doesn't have any letter in it - function should return True.
+
+Input: A string.
+Output: a boolean.
+
+Example:
+
+is_all_upper('ALL UPPER') == True
+is_all_upper('all lower') == False
+is_all_upper('mixed UPPER and lower') == False
+is_all_upper('') == True
+'''
+
+def is_all_upper(txt: str) -> bool:
+    if len(txt) > 0 and txt.isalpha():
+        for words in txt.split(" "):
+            return False if words.isupper() is False else True
+
+    return True
+
+if __name__ == '__main__':
+    print("Example:")
+    print(is_all_upper('ALL UPPER'))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert is_all_upper('ALL UPPER') == True
+    assert is_all_upper('all lower') == False
+    assert is_all_upper('mixed UPPER and lower') == False
+    assert is_all_upper('  ') == True
+    print("Coding complete? Click 'Check' to earn cool rewards!")
