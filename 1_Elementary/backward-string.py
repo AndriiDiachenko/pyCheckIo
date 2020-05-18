db86bccbef53b44c22037d973f256955376b8825
Dandrewd <amdiachenko@gmail.com>
2020-05-18 22:02:19 +0300

    Simple [::-1]

---
+++ 1_Elementary/backward-string.py
@@ -0,0 +1,18 @@
+'''
+You should return a given string in reverse order.
+
+Input: A string.
+
+Output: A string.
+
+Example:
+
+backward_string('val') == 'lav'
+backward_string('') == ''
+backward_string('ohho') == 'ohho'
+backward_string('123456789') == '987654321'
+'''
+
+def backward_string(txt: str) -> str:
+    return txt[::-1]
+
