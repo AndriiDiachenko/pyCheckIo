8d47493eeb35cded18a111bf6561a3d63683e48d
Dandrewd <amdiachenko@gmail.com>
2020-05-18 20:03:09 +0300

    strange task with chr()

--- 0_Home/pawn-brotherhood.py
+++ 0_Home/pawn-brotherhood.py
@@ -8,14 +8,21 @@ Output: The number of safe pawns as a integer.
 
 Example:
 
-safe_pawns({"b4", "d4", "f4", "c3", "e3", "g5", "d2"}) == 6
+safe_pawns({"a1", "d4", "f4", "c3", "e3", "g5", "d2"}) == 6
 safe_pawns({"b4", "c4", "d4", "e4", "f4", "g4", "e5"}) == 1
 '''
 
 def safe_pawns(pawns: set) -> int:
-    pawns_cordinates = tuple((ord(i[0]) - 96, int(i[1])) for i in pawns)
-    print(pawns_cordinates)
+
+    safe_pown = 0
+    for pawn in pawns:
+        if chr(ord(pawn[0]) - 1) + str(int(pawn[1]) - 1) in pawns or chr(ord(pawn[0]) + 1) + str(
+            int(pawn[1]) - 1) in pawns:
+            safe_pown += 1
+    return safe_pown
 
 
 
-safe_pawns({"b4", "d4", "f4", "c3", "e3", "g5", "d2"})
+
+
+print(safe_pawns({"a4", "d4", "f4", "c3", "e3", "g5", "d2"}))
