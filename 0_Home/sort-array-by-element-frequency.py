82371e2ea34c028a37c6f6cc76a825221f3ddc5e
Dandrewd <amdiachenko@gmail.com>
2020-05-18 12:08:49 +0300

    minor changes

--- 0_Home/sort-array-by-element-frequency.py
+++ 0_Home/sort-array-by-element-frequency.py
@@ -3,15 +3,14 @@ Sort the given iterable so that its elements end up in the decreasing frequency 
 If two elements have the same frequency, they should end up in the same order as the first appearance in the iterable.
 
 Input: Iterable
-
 Output: Iterable
 
 Example:
-
 frequency_sort([4, 6, 2, 2, 6, 4, 4, 4]) == [4, 4, 4, 4, 6, 6, 2, 2]
 frequency_sort(['bob', 'bob', 'carl', 'alex', 'bob']) == ['bob', 'bob', 'bob', 'carl', 'alex']
 
-Precondition: elements can be ints or strings'''
+Precondition: elements can be ints or strings
+'''
 
 def frequency_sort(array: list) -> list:
     res = sorted(array, key=lambda x: (-array.count(x), array.index(x)))
